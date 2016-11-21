ae080ea2a26b4a0a8fa41d68108e9c46e2cd6c6b
vimalkumar1993 <vimalkumar1993@gmail.com>
2016-11-21 13:45:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,322 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+        <!-- ========== VIEWPORT META ========== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+        <!-- ========== PAGE TITLE ========== -->
+        <?php include('common/title.php'); ?>
+
+        <!-- ========== META TAGS ========== -->
+        <meta name="description" content="HTML template for Software, Mobile App & SaaS applications.">
+        <meta name="keywords" content="software, coimbatore, offline software, forum, video streaming, college project, quiz, competative exam coaching software, quiz software">
+        <meta name="author" content="Vimal">
+
+        <!-- ========== FAVICON & APPLE ICONS ========== -->
+        <link rel="shortcut icon" href="images/favicon.png">
+        <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+
+        <!-- ========== MINIFIED VENDOR CSS ========== -->
+        <link rel="stylesheet" href="styles/vendor.css">
+        <link rel="stylesheet" href="styles/main.css">
+
+        <!-- ========== MODERNIZR ========== -->
+    	<script src="scripts/vendor/modernizr.js"></script>
+    </head>
+
+    <!-- ========== BODY ==========
+    .light-header: for light colored header
+    .dark-header: for dark colored header
+    ==========  ========== -->
+    <body class="light-header animsition">
+
+        <!-- ========== NAVIGATION ========== -->
+<?php  include('common/header.php');?>
+
+        <!-- ========== HEADER ========== -->
+        <header class="header main-header header-style-2" id="header-animated">
+            <div class="primary-trans-bg">
+                <div class="container">
+                    <!-- For centering the content vertically -->
+                    <div class="outer">
+                        <div class="inner text-center">
+                            <h1 class="white-color">Create Website That Will Impress Your Customers.</h1>
+                            <h5 class="">One of the best HTML templates for Software, Mobile App or SaaS product.</h5>
+                            <a href="#" class="btn se-btn-white btn-rounded">Free 30 Days Trial</a>
+                        </div> <!-- end inner -->
+                    </div> <!-- end outer -->
+                </div> <!-- end container -->
+            </div> <!-- end primary-trans-bg -->
+        </header>
+
+        <!-- ========== FEATURES ========== -->
+        <section class="se-section features-section">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-3 col-sm-6 se-feature">
+                        <i class="icon ion-ios-analytics-outline"></i>
+                        <h5>Fully Responsive</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                    </div> <!-- end se-feature -->
+
+                    <div class="col-md-3 col-sm-6 se-feature">
+                        <i class="icon ion-ios-paper-outline"></i>
+                        <h5>Well Documented</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                    </div> <!-- end se-feature -->
+
+                    <div class="col-md-3 col-sm-6 se-feature">
+                        <i class="icon ion-ios-lightbulb-outline"></i>
+                        <h5>Easy To Use</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                    </div> <!-- end se-feature -->
+
+                    <div class="col-md-3 col-sm-6 se-feature">
+                        <i class="icon ion-ios-speedometer-outline"></i>
+                        <h5>Lightning Fast</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                    </div> <!-- end se-feature -->
+
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+        </section>
+
+        <!-- ========== SINGLE FEATURE SECTION ========== -->
+        <section class="se-section single-feature">
+
+            <div class="col-md-6 zero-padding col-md-push-6 side-img">
+                <img src="images/dashboard-img-3.png" alt="" class="img-responsive">
+            </div> <!-- end side-img -->
+
+            <div class="container feature-desc">
+                <div class="row">
+                    <div class="col-md-5 feature-info">
+                        <h2>This is amazing dashboard</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud officia deserunt mollit exercitation.</p>
+                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui anim id est laborum.</p>
+                        <a href="#" class="btn se-btn btn-rounded">Learn More</a>
+                    </div>
+
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+
+        </section>
+
+        <!-- ========== SINGLE FEATURE SECTION ========== -->
+        <section class="se-section single-feature">
+
+            <div class="col-md-6 zero-padding side-img">
+                <img src="images/dashboard-img-3.png" alt="" class="img-responsive">
+            </div> <!-- end side-img -->
+
+            <div class="container feature-desc">
+                <div class="row">
+                    <div class="col-md-5 col-md-push-7 feature-info">
+                        <h2>This is amazing dashboard</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud officia deserunt mollit exercitation.</p>
+                        <p>Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui anim id est laborum.</p>
+                        <a href="#" class="btn se-btn btn-rounded">Learn More</a>
+                    </div>
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+        </section>
+
+        <!-- ========== STEPS ========== -->
+        <section class="se-section se-steps">
+            <div class="container">
+                <div class="row text-center">
+                    <h3 class="underline mtn">It's as ease as 1 - 2 - 3!</h3>
+                </div> <!-- end row -->
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 one-step">
+                        <h4>Sign Up</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4 one-step">
+                        <h4>Add Users</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+
+                    <div class="col-md-4 col-sm-4 one-step">
+                        <h4>Assign Tasks</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+        </section>
+
+        <!-- ========== FEATURES ========== -->
+        <section class="se-section features-section parallax-bg" data-parallax="scroll" data-image-src="images/bg-img-3.jpg" data-speed="0.4">
+            <div class="black-gradient">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6 se-feature-style-2 mb60">
+                            <div class="left">
+                                <i class="icon ion-arrow-expand"></i>
+                            </div>
+                            <div class="right">
+                                <h5>Fully Responsive</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                            </div>
+                        </div> <!-- end se-feature -->
+
+                        <div class="col-md-6 col-sm-6 se-feature-style-2 mb60">
+                            <div class="left">
+                                <i class="icon ion-android-search"></i>
+                            </div>
+                            <div class="right">
+                                <h5>Retina Ready</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                            </div>
+                        </div> <!-- end se-feature -->
+
+                        <div class="col-md-6 col-sm-6 se-feature-style-2 mb60">
+                            <div class="left">
+                                <i class="icon ion-ios-bolt"></i>
+                            </div>
+                            <div class="right">
+                                <h5>Lightning Fast</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                            </div>
+                        </div> <!-- end se-feature -->
+
+                        <div class="col-md-6 col-sm-6 se-feature-style-2 mb60">
+                            <div class="left">
+                                <i class="icon ion-information"></i>
+                            </div>
+                            <div class="right">
+                                <h5>Google Fonts</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                            </div>
+                        </div> <!-- end se-feature -->
+
+                        <div class="col-md-6 col-sm-6 se-feature-style-2">
+                            <div class="left">
+                                <i class="icon ion-checkmark"></i>
+                            </div>
+                            <div class="right">
+                                <h5>Light Weight</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                            </div>
+                        </div> <!-- end se-feature -->
+
+                        <div class="col-md-6 col-sm-6 se-feature-style-2">
+                            <div class="left">
+                                <i class="icon ion-settings"></i>
+                            </div>
+                            <div class="right">
+                                <h5>Easy To Edit</h5>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut.</p>
+                            </div>
+                        </div> <!-- end se-feature -->
+
+                    </div> <!-- end row -->
+                </div> <!-- end container -->
+            </div> <!-- end black-gradient -->
+        </section> <!-- end features-section -->
+
+        <!-- ========== CTA SECTION ========== -->
+        <section class="se-section primary-bg">
+            <div class="container">
+                <div class="row text-center">
+                    <h3>Create Your Account Now!</h3>
+                    <p>Get 30 days trial. No credit card required.</p>
+                    <a href="#" class="btn se-btn-black btn-rounded">SIGN UP</a>
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+        </section>
+
+        <!-- ========== FOOTER ========== -->
+        <footer class="light-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <img src="images/dark-logo.svg" alt="" class="footer-logo">
+                    </div>
+
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <h6 class="mtn">HOME</h6>
+                        <ul>
+                            <li><a href="#">Parallax Background</a></li>
+                            <li><a href="#">Animated Background</a></li>
+                            <li><a href="#">Solid Color</a></li>
+                            <li><a href="#">Gradient Background</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <h6 class="mtn">PAGES</h6>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Our Team</a></li>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#">Features</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-2 col-sm-3 col-xs-6">
+                        <h6 class="mtn">OTHER</h6>
+                        <ul>
+                            <li><a href="#">Sign Up</a></li>
+                            <li><a href="#">Login</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Terms of Service</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-4 col-sm-8 col-sm-offset-2 col-md-offset-0">
+                        <h6 class="mtn">SUBSCRIBE</h6>
+                        <div class="input-group">
+                            <input type="email" class="form-control inp-rounded" placeholder="Enter your email id">
+                            <span class="input-group-btn">
+                                <button class="btn se-btn btn-rounded" type="button">Subscribe!</button>
+                            </span> <!-- end input-group-btn -->
+                        </div><!-- end input-group -->
+                        <p class="text-center text-muted">We don't spam!</p>
+                    </div>
+                </div> <!-- end row -->
+
+                <div class="row footer-bottom">
+                    <div class="col-md-6">
+                        <p>Copyright &copy; SoftEase Technologies. 2016. All Rights Reserved.</p>
+                    </div>
+
+                    <div class="col-md-6 text-right">
+                        <h6><a href="#">FACEBOOK</a></h6>
+                        <h6><a href="#">TWITTER</a></h6>
+                        <h6><a href="#">LINKEDIN</a></h6>
+                        <h6><a href="#">GOOGLE PLUS</a></h6>
+                    </div>
+                </div> <!-- end footer-bottom -->
+            </div> <!-- end container -->
+        </footer>
+
+
+    	<script src="scripts/vendor.js"></script>
+
+        <!-- ========== MINIFIED PLUGINS JS ========== -->
+    	<script src="scripts/plugins.js"></script>
+
+    	<script src="scripts/main.js"></script>
+
+        <script src="scripts/init-animation.js"></script>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','../../www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-75815870-4', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
